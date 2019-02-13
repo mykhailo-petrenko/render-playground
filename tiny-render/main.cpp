41bc5402813c011817935c23b12ed680f59a937d
Mykhailo Petrenko <mihalytch@gmail.com>
2019-02-13 22:56:57 +0200

    Initial Commit

---
+++ tiny-render/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tiny-render
+//
+//  Created by Mykhailo Petrenko on 2/13/19.
+//  Copyright © 2019 Mykhailo Petrenko. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
